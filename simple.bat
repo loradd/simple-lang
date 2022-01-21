23eb3c9b322ebffd54e53bd6dec025a1b84fa2cb
Lorenzo Addazi <addazi.lorenzo@gmail.com>
2022-01-21 20:39:38 +0100

    Removed windows script + Possible JAVA_TOOL_OPTIONS picked up messages suppressed in simple script
    Signed-off-by: Lorenzo Addazi <addazi.lorenzo@gmail.com>

--- simple.bat
+++
@@ -1,8 +0,0 @@
-@if "%DEBUG%"=="" @echo off
-
-if "%OS%"=="Windows_NT" setlocal EnableDelayedExpansion
-
-set ARGS=%*
-call gradlew --quiet --console=plain installDist && call "build\install\simple\bin\simple" %ARGS%
-
-if "%OS%"=="Windows_NT" endlocal
